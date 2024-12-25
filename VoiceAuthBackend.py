ba9f93f848b1ab1a0d494ccd6df4a6ef36eccd45
sadiq kassamali <Sadiq kassamali@gmail.com>
2024-12-25 04:22:00 -0600

    Streamlit version of the VoiceAuth APP

--- VoiceAuthBackend.py
+++ VoiceAuthBackend.py
@@ -19,7 +19,7 @@ import datetime
 import logging
 import os
 from torch import hub
-from transformers import pipeline, Pipeline
+from transformers import pipeline
 
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
