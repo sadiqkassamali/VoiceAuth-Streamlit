152c1762791cb040fef55ea1edd7c5282570b686
sadiq kassamali <Sadiq kassamali@gmail.com>
2024-12-25 04:15:27 -0600

    Streamlit version of the VoiceAuth APP

--- streamliit_VoiceAuth.py
+++ streamliit_VoiceAuth.py
@@ -9,7 +9,7 @@ import streamlit as st
 from PIL import Image
 from tempfile import NamedTemporaryFile
 from audioread.exceptions import NoBackendError
-from moviepy import AudioFileClip
+from moviepy import AudioFileClipa
 from moviepy.editor import *
 
 from VoiceAuthBackend import (
